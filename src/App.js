50419070a22d474612f9a406472d6bbc7583f4e5
TeamxAlpha <zohaib104006@gmail.com>
2023-07-21 18:18:58 +0500

    add the checkbox, edit and delete

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
-  const [editMode, setEditMode] = useState(false);
   const [selectedTask, setSelectedTask] = useState('');
+  
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
   };
@@ -21,28 +21,36 @@ function App() {
 
   const deleteTask = (task) => {
     setTasks((tasks) => tasks.filter((item) => item !== task));
+  
   };
 
   const editTask = (task) => {
-    setEditMode(true);
     setNewTask(task);
     setSelectedTask(task);
+  };
+
+  const cutTask = (task) => {
+    setTasks((tasks) => 
+    tasks.map((item)=>
+    item === task ? newTask : item
+      )
+    );
   };
 
   const saveEditedTask = () => {
     setTasks((tasks) => tasks.map((item) => (item === selectedTask ? newTask : item)));
     setNewTask('');
-    setEditMode(false);
+    setSelectedTask('');
   };
 
   return (
     <div className="App">
-      <h1 style={{ color: 'white' }}>Todo App</h1>
+      <h1>📝 Todo App</h1>
       <TaskInput
         value={newTask}
         onChange={handleInputChange}
         onAddTask={addTask}
-        isEditMode={editMode}
+        isEditMode={!!selectedTask}
         onEditTask={saveEditedTask}
       />
       <ol>
@@ -50,9 +58,10 @@ function App() {
           <TaskItem
             key={index}
             task={task}
-            index={index}
+            index={index+1}
             deleteTask={deleteTask}
             editTask={editTask}
+            cutTask={cutTask}
           />
         ))}
       </ol>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,14 +1,26 @@
+import React, { useState } from 'react';
 
+function TaskItem  ({ task, index, deleteTask, editTask}) {
 
-import React from 'react';
+  const [check, setcheck] = useState (false);
 
-function TaskItem({ task, index, deleteTask, editTask }) {
+  let className = check ? "cut" : "";
+
   return (
-    <li onClick={() => editTask(task)}>
-      {index}. {task}
-      <button onClick={() => deleteTask(task)}>Delete</button>
+    <div className="container">
+    <input type="checkbox" onClick={()=> setcheck(!check)} ></input>
+    <li className={className}  onClick={() => editTask(task)}>
+      {index}.{task}
+      <div className="listitems">
+      {/* <button className="btn" >🖊️</button> */}
+      <img src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png" onClick={() => editTask(task)} class="edit" alt="Edit" data-id="2"></img>
+      {/* <button className="btn" onClick={() => deleteTask(task)}>🗑️</button> */}
+      <img src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png"onClick={() => deleteTask(task)} class="delete" alt="delete" data-id="2"></img>
+      </div>
+
     </li>
-  );
+    </div>  );
+
 }
 
 export default TaskItem;
